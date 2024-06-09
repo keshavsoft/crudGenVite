0efabf83b58dc70c14766fe11b8199bba3ddc5aa
Keshav <nknnkumar@yahoo.com>
2024-06-09 14:30:10 +0530

    keshav

---
+++ Projects/KWSServer/OnMessage/EntryFile.js
@@ -0,0 +1,19 @@
+import { StartFunc as CommonMessageAsJson } from "./MessageAsJson/EntryFile.js";
+import { StartFunc as CommonMessageAsString } from "./MessageAsString/EntryFile.js";
+
+let StartFunc = ({ inData, inws, inClients, inWss }) => {
+    let LocalData = inData;
+    
+    try {
+        let LocalDataAsJson = JSON.parse(LocalData);
+     
+        CommonMessageAsJson({ inDataAsJson: LocalDataAsJson, inws, inClients, inWss });
+
+        return;
+    } catch (error) {
+    };
+
+    CommonMessageAsString({ inDataAsString: LocalData.toString(), inws, inClients });
+};
+
+export { StartFunc };
